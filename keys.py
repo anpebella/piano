61b3b30abb0f677ad764d6d033158d35e2a433f2
anpebella <olegpetrovich83@gmail.com>
2025-10-24 23:49:19 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,16 +2,18 @@ from pygame import *
 from settings import *
 from effects import draw_key_effect
 
-def create_but(x=start_x, y=start_y, width=but_width, height=but_height):
+def create_but(num_keys,x=start_x, y=start_y, width=but_width, height=but_height):
     rects = []
-    for key in keys.keys():
+    for key in range(num_keys):
         rect = Rect(x, y, width, height)
-        x += 90
+        x += 80
         rects.append(rect)
     return rects
 
-
-def blit_rect(window, rects, pressed_rect):
+def blit_rect(window, rects, pressed_rect,x,y):
     for i,rect in enumerate(rects):
         pressed=i in pressed_rect
-        draw_key_effect(window,rect,pressed)
+        draw_key_effect(window,rect,x,y,i,pressed)
+        rect_text = fonte.render(text[i], True, 'black')
+        window.blit(rect_text,(rect.x+10,rect.y+100))
+
